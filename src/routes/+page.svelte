858ae5936ea87d89d3a07da2b96e3d7f8ba5a398
Himan-Miku <himanshu28.mj@gmail.com>
2023-02-09 21:20:10 +0530

    dynamic routes adn folder structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex justify-center items-center h-screen">
+    <h1 class="text-black text-3xl font-bold">Hello World</h1>
+</div>
+
---
+++ src/routes/products/[productId]/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+    import { page } from "$app/stores"
+    const productId = $page.params.productId
+</script>
+
+<h1 class="text-3xl font-semibold text-blue-600 text-center my-4"> {productId} Product Details Page</h1>
