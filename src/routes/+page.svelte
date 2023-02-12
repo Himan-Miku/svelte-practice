4a9a26ed778acdd3074aa9b9b31f6bdf7117676a
Himan-Miku <himanshu28.mj@gmail.com>
2023-02-12 23:19:49 +0530

    optional params

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,3 @@
 <div class="flex justify-center items-center h-screen">
-    <h1 class="text-black text-3xl font-bold">Hello World</h1>
+	<h1 class="text-black text-3xl font-bold">Root Page</h1>
 </div>
-
---
+++ src/routes/marketing/[[lang]]/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+	import { page } from '$app/stores';
+	const greetings = {
+		en: 'Hello!',
+		sp: 'Hola!',
+		fr: 'Bonjour'
+	};
+	const { lang = 'en' } = $page.params;
+	const greeting = greetings[lang];
+	console.log(greeting);
+</script>
+
+{#if greeting === undefined}
+	<div class="flex justify-center items-center">
+		<h1
+			class="text-center font-bold text-white p-4 inline-block rounded-xl bg-red-400 text-4xl my-4"
+		>
+			Sorry This Language Is Not Supported
+		</h1>
+	</div>
+{:else}
+	<h1 class="text-center font-bold text-blue-400 text-4xl my-4">{greeting}</h1>
+{/if}
