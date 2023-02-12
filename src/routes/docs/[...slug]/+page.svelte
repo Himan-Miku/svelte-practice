6f66dd86f2fca6a10a069a20acd806d5723eaa5a
Himan-Miku <himanshu28.mj@gmail.com>
2023-02-12 22:55:46 +0530

    catch all routes with if else ladder in svelte

--- src/routes/docs/[...slug]/+page.svelte
+++ src/routes/docs/[...slug]/+page.svelte
@@ -1,1 +1,16 @@
-<h1 class="text-center font-semibold text-red-400 text-4xl">Docs Home Page</h1>
+<script>
+	import { page } from '$app/stores';
+	console.log($page.params.slug);
+	const slugs = $page.params.slug.split('/');
+</script>
+
+{#if slugs.length === 1}
+	<h3 class="text-center font-semibold text-red-400 text-2xl my-4">
+		Viewing docs for feature {slugs[0]}
+	</h3>
+{:else if slugs.length === 2}
+	<h3 class="text-center font-semibold text-red-400 text-2xl my-4">
+		Viewing docs for feature {slugs[0]} and concept {slugs[1]}
+	</h3>
+{/if}
+<h1 class="text-center font-semibold text-red-400 text-4xl my-4">Docs Home Page</h1>
