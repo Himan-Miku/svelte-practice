2be99845fff361d52b0c0d7357da5cbad36be1f3
Himan-Miku <himanshu28.mj@gmail.com>
2023-02-14 00:04:27 +0530

    basic navigation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,11 @@
+<script>
+	import { goto } from "$app/navigation"
+	const navigate = () => { 
+		goto('/products')
+	}
+</script>
+
 <div class="flex justify-center items-center h-screen">
 	<h1 class="text-black text-3xl font-bold">Root Page</h1>
+	<button class="bg-blue-400 rounded-xl p-2 font-semibold text-lg text-white ml-6" on:click={navigate}>Place Order</button>
 </div>
--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -1,1 +1,19 @@
+<script>
+    import { goto } from "$app/navigation"
+    const handleClick1 = () => {
+        goto('/products/product1')
+    }
+    const handleClick2 = () => {
+        goto('/products/product2')
+    }
+    const handleClick3 = () => {
+        goto('/products/product3')
+    }
+</script>
+
 <h1 class="text-4xl my-4 text-center text-blue-500 font-bold">Products page</h1>
+<ul class="flex gap-6 ml-8">
+    <li on:click={handleClick1} class="bg-red-400 rounded-md p-2 text-black font-medium"> Product 1 </li>
+    <li on:click={handleClick2} class="bg-red-400 rounded-md p-2 text-black font-medium"> Product 2 </li>
+    <li on:click={handleClick3} class="bg-red-400 rounded-md p-2 text-black font-medium"> Product 3 </li>
+</ul>
